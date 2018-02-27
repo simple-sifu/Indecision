1fffdb62babbb064ca743842e1f8b962325805d8
tommy.han <tommy.han@dealertrack.com>
2018-02-27 01:25:15 -0500

    React with JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,14 @@
 console.log("app.js is running")
+
+// JSX - Javascript XML
+//var template = <p>This is JSX from app.js !</p>;
+
+var template = React.createElement(
+    'p',
+    {id: 'someid'},
+    "This is JSX from app.js"
+);
+var appRoot = document.getElementById('app');
+
+
+ReactDOM.render(template, appRoot)
