641d02296c7675eca6e6493fb3bb6a9db8f0f238
tommy.han <tommy.han@dealertrack.com>
2018-02-27 22:50:03 -0500

    using Babel to translate JSX to Javascript

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,13 @@
-console.log("app.js is running")
+"use strict";
+
+console.log("app.js is running");
 
 // JSX - Javascript XML
-//var template = <p>This is JSX from app.js !</p>;
-
 var template = React.createElement(
-    'p',
-    {id: 'someid'},
-    "This is JSX from app.js"
+  "p",
+  null,
+  "This is JSX from app.js !"
 );
 var appRoot = document.getElementById('app');
 
-
-ReactDOM.render(template, appRoot)
+ReactDOM.render(template, appRoot);
