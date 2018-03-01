897c41eab6785ae081c14d70e42aa5220723b230
tommy.han <tommy.han@dealertrack.com>
2018-02-28 23:32:43 -0500

    scripts output

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,9 +4,32 @@ console.log("app.js is running");
 
 // JSX - Javascript XML
 var template = React.createElement(
-  "p",
-  null,
-  "This is JSX from app.js !"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Indecision App"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "This is some info"
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Item One"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Item Two"
+        )
+    )
 );
 var appRoot = document.getElementById('app');
 
