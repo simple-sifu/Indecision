c8e255540770a47b927743532d24b3b9fa48b097
tommy.han <tommy.han@dealertrack.com>
2018-03-03 01:12:51 -0500

    using jsx expressions

--- src/app.js
+++ src/app.js
@@ -23,11 +23,17 @@ var user = {
     location: 'New York'
 };
 
+function getLocation(){
+    if (location){
+        return <p>Location: {location}</p>
+    }
+}
+
 var template2 = (
     <div>
-        <h1>{user.name}</h1>
-        <p>Age: {user.age}</p>
-        <p>Location: {user.location}</p>
+        <h1>{user.name ? user.name : "Anonymous" }</h1>
+        { (user.age && user.age >= 18 ) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
         <ol>
             <li>Item One</li>
             <li>Item Two</li>
@@ -37,4 +43,4 @@ var template2 = (
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
