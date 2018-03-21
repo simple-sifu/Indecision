2a73fb41a823e3f2171b5d847589dd8545883c7b
tommy.han <tommy.han@dealertrack.com>
2018-03-21 17:56:31 -0400

    es6 functions and how the this keyword is no longer bound

--- src/playground/es6-arrow-function-2.js
+++ src/playground/es6-arrow-function-2.js
@@ -1,34 +1,35 @@
-'use strict';
-
 // argument objects - no longer bound with arrow functions
 
-var add = function add(a, b) {
+const add = function (a, b){
     console.log(arguments);
     return a + b;
-};
-console.log(add(50, 60, 70));
+}
+console.log(add(50, 60, 70))
 
-var addArrow = function addArrow(a, b) {
+
+const addArrow =  (a, b) => {
     // Es6 no longer supports arguments !!!
     // console.log(arguments);
     return a + b;
-};
-console.log(addArrow(10, 20, 30));
+}
+console.log(addArrow(10, 20, 30))
+
 
 // this keyword - no longer bound
 
-var user = {
+const user = {
     name: 'Andrew',
     cities: ['Philadelphia', 'New York', 'Dublin'],
     // if function is set to object.property it is bound to the object,
     // but if just define an anonymous function within a function that function is not be bound to the object.
-    printPlacesLived: function printPlacesLived() {
+    printPlacesLived: function(){
         console.log(this.name);
         console.log(this.cities);
-        var that = this;
-        this.cities.forEach(function (city) {
-            console.log(that.name, ' has lived in ', city);
+        const that = this
+        this.cities.forEach(function (city){
+            console.log(that.name, ' has lived in ', city)
         });
+
     }
 };
 user.printPlacesLived();
