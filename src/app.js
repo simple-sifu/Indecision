d62e860fa183ce194693210e2b3ee804e151c0b7
tommy.han <tommy.han@dealertrack.com>
2018-03-22 00:52:01 -0400

    move counter to playground

--- src/app.js
+++ src/app.js
@@ -19,36 +19,6 @@ const template = (
     </div>
 );
 
-let count = 0
-
-const addOne = () => {
-    count++;
-    renderCounterApp();
-};
-
-const minusOne = () => {
-    count--;
-    renderCounterApp();
-};
-
-const reset = () => {
-    count = 0;
-    renderCounterApp();
-};
-
 const appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot);
 
-const renderCounterApp = () => {
-    const template2 = (
-        <div>
-        <h1>Count: {count}</h1>
-        <button onClick={addOne}> +1 </button>
-        <button onClick={minusOne}> -1 </button>
-        <button onClick={reset}>Reset </button>
-        </div>
-    )
-
-    ReactDOM.render(template2, appRoot);
-}
-
-renderCounterApp();
