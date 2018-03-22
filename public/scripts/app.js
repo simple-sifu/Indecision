31cbcbe91e1cfaf4b1e50b375e30daee7c8a47f6
tommy.han <tommy.han@dealertrack.com>
2018-03-22 00:48:19 -0400

    manual data binding

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,54 +1,94 @@
 'use strict';
 
-// argument objects - no longer bound with arrow functions
+console.log("app.js is running");
 
-var add = function add(a, b) {
-    console.log(arguments);
-    return a + b;
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
 };
-console.log(add(50, 60, 70));
 
-var addArrow = function addArrow(a, b) {
-    // Es6 no longer supports arguments !!!
-    // console.log(arguments);
-    return a + b;
+var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options.length > 0 ? 'Here are your options' : 'No options'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item One'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item Two'
+        )
+    )
+);
+
+var count = 0;
+
+var addOne = function addOne() {
+    count++;
+    renderCounterApp();
 };
-console.log(addArrow(10, 20, 30));
 
-// this keyword - no longer bound
-
-var user = {
-    name: 'Andrew',
-    cities: ['Philadelphia', 'New York', 'Dublin'],
-    // if function is set to object.property it is bound to the object,
-    // but if just define an anonymous function within a function that function is not be bound to the object.
-
-    // can not use arrow function for function that maps to property but
-    // we can use a ES6 method that will work with this keyword
-    printPlacesLived: function printPlacesLived() {
-        var _this = this;
-
-        return this.cities.map(function (city) {
-            return _this.name + ' has lived in ' + city;
-        });
-    }
+var minusOne = function minusOne() {
+    count--;
+    renderCounterApp();
 };
-console.log(user.printPlacesLived());
 
-// Challenge Area
-// numbers - array of numbers
-// multiplyBy - single number
-// multiply - returns a new array where the number have been multiplied
-
-var multiplier = {
-    numbers: [10, 20, 30, 40, 50],
-    multiplyBy: 4,
-    multiply: function multiply(multiplyBy) {
-        var _this2 = this;
-
-        return this.numbers.map(function (number) {
-            return number * _this2.multiplyBy;
-        });
-    }
+var reset = function reset() {
+    count = 0;
+    renderCounterApp();
 };
-console.log(multiplier.multiply());
+
+var appRoot = document.getElementById('app');
+
+var renderCounterApp = function renderCounterApp() {
+    var template2 = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            'Count: ',
+            count
+        ),
+        React.createElement(
+            'button',
+            { onClick: addOne },
+            ' +1 '
+        ),
+        React.createElement(
+            'button',
+            { onClick: minusOne },
+            ' -1 '
+        ),
+        React.createElement(
+            'button',
+            { onClick: reset },
+            'Reset '
+        )
+    );
+
+    ReactDOM.render(template2, appRoot);
+};
+
+renderCounterApp();
--- src/app.js
+++ src/app.js
@@ -19,29 +19,36 @@ const template = (
     </div>
 );
 
-const user = {
-    name: 'Andrew',
-    age: 26,
-    location: 'New York'
+let count = 0
+
+const addOne = () => {
+    count++;
+    renderCounterApp();
 };
 
-function getLocation(){
-    if (location){
-        return <p>Location: {location}</p>
-    }
-}
+const minusOne = () => {
+    count--;
+    renderCounterApp();
+};
 
-const template2 = (
-    <div>
-        <h1>{user.name ? user.name : "Anonymous" }</h1>
-        { (user.age && user.age >= 18 ) && <p>Age: {user.age}</p>}
-        {getLocation(user.location)}
-        <ol>
-            <li>Item One</li>
-            <li>Item Two</li>
-        </ol>
-    </div>
-);
+const reset = () => {
+    count = 0;
+    renderCounterApp();
+};
 
 const appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+
+const renderCounterApp = () => {
+    const template2 = (
+        <div>
+        <h1>Count: {count}</h1>
+        <button onClick={addOne}> +1 </button>
+        <button onClick={minusOne}> -1 </button>
+        <button onClick={reset}>Reset </button>
+        </div>
+    )
+
+    ReactDOM.render(template2, appRoot);
+}
+
+renderCounterApp();
